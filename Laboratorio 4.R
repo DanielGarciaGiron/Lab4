3c32a5b52a3ab13827740de87fabb16fe450fc0a
DannyGarcia <daniel.garcia.giron@gmail.com>
2018-10-03 10:32:09 -0600

    final commit

--- Laboratorio 4.R
+++ Laboratorio 4.R
@@ -4,7 +4,7 @@
 #------------------------------------------------------------#
 
 #----- Instalación e importación de librerias necesarias para correr el programa ----#
-for (libreria in c("stringr","tm", "R.utils","openNLP","rJava","qdap","RWeka","ggplot2")) {
+for (libreria in c("stringr","tm", "R.utils","openNLP","rJava","qdap","RWeka","ggplot2","stylo")) {
   if (!require(libreria, character.only=T)) {
     install.packages(libreria)
     library(libreria, character.only=T)
@@ -141,3 +141,101 @@ PlotFrec(FrecBigrama, "Bigramas más comúnes (Top 25)")
 #Trigramas
 PlotFrec(FrecTrigrama, "Trigramas más comúnes (Top 25)")
 
+
+
+predictNextWord <- function(input)
+{
+  #Cleaning the input
+  wordInput <- cleanInput(input)
+  #Getting the number of words in the input
+  wordCount <- length(wordInput)
+  #Initializing response
+  prediction <- c()
+  
+  #Trimming input to the last three words
+  if(wordCount>3)
+  {
+    wordInput <- wordInput[(wordCount-2):wordCount]
+    prediction <- matchinFourGranm(wordInput[1],wordInput[2],wordInput[3])
+  }
+  #Three Gram Match
+  if(wordCount ==2)
+  {
+    prediction <- matchThreeGram(wordInput[1],wordInput[2])
+  }
+  #Two gram match
+  if(wordCount ==1)
+  {
+    prediction <- matchTwoGram(wordInput[1])
+  }
+  
+  #No word entered
+  if(wordCount == 0)
+  {
+    prediction <- "Casilla vacia"
+  }
+  
+  #Unknown words
+  if(length(prediction)==0)
+  {
+    prediction <- "Oops!!! unfortunately  I was not able to make sense of what you told me"
+  }
+  
+  #Returning response
+  if(length(prediction) < 5)
+  {
+    prediction
+  }
+  else
+  {
+    prediction[1:5]
+  }
+}
+
+
+
+#Cleaning input to extract specific words
+cleanInput <- function(text){
+  textInput <- tolower(text)
+  textInput <- removePunctuation(textInput)
+  textInput <- removeNumbers(textInput)
+  textInput <- str_replace_all(textInput, "[^[:alnum:]]", " ")
+  textInput <- stripWhitespace(textInput)
+  textInput <- txt.to.words.ext(textInput, language="English.all", preserve.case = TRUE)
+  return(textInput)
+}
+
+
+
+
+
+#Match string in Three Gram and get probable word
+matchThreeGram <- function(inputWord1,inputWord2)
+{
+  predictWord <- filter(Trigrama,( word1 == inputWord1 & word2 == inputWord2))$word3
+  if(length(predictWord)==0)
+  {
+    predictWord <- filter(Trigrama,(word2 == inputWord2))$word3 
+    
+    if(length(predictWord)== 0)
+    {
+      predictWord <- filter(Trigrama,(word1 == inputWord2))$word2 
+      
+      if(length(predictWord) ==0 )
+      {
+        predictWord <- matchTwoGram(inputWord2)
+      }
+      
+    }
+  }
+  predictWord
+}
+
+#Match string in Two Gram and get probable word
+matchTwoGram <- function(inputWord1)
+{
+  predictWord <- filter(Bigrama,( word1 == inputWord1 ))$word2
+  
+  predictWord
+  
+}
