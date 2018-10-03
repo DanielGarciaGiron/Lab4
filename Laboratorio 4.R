aa9de45b786deefdc85e9f6d36f073a8e1a396c0
Rodrigo Chavez <rodrich266@gmail.com>
2018-10-03 02:04:55 -0600

    Tokenización y Ngramas

--- Laboratorio 4.R
+++ Laboratorio 4.R
@@ -4,7 +4,7 @@
 #------------------------------------------------------------#
 
 #----- Instalación e importación de librerias necesarias para correr el programa ----#
-for (libreria in c("stringr","tm", "R.utils","openNLP","rJava","qdap","tm","RWeka")) {
+for (libreria in c("stringr","tm", "R.utils","openNLP","rJava","qdap","RWeka","ggplot2")) {
   if (!require(libreria, character.only=T)) {
     install.packages(libreria)
     library(libreria, character.only=T)
@@ -13,23 +13,23 @@ for (libreria in c("stringr","tm", "R.utils","openNLP","rJava","qdap","tm","RWek
 
 #---------------------- Lectura de Datos --------------------------------------------#
 TwitterData <- "./data/en_US.twitter.txt"
-Twitter <- scan(TwitterData, character(0), sep = "\n") 
-lineasTwitter<-as.numeric(countLines(TwitterData))
+Twitter <- scan(TwitterData, character(0), sep = "\n", encoding="latin1", skipNul = TRUE)
+LineasTwitter<-as.numeric(countLines(TwitterData))
 
 
 NewsData <- "./data/en_US.news.txt"
-News <- scan(NewsData, character(0), sep = "\n") 
-lineasNews<-as.numeric(countLines(NewsData))
+News <- scan(NewsData, character(0), sep = "\n", encoding="latin1", skipNul = TRUE)
+LineasNews<-as.numeric(countLines(NewsData))
 
 
 BlogsData <- "./data/en_US.blogs.txt"
-Blogs <- scan(BlogsData, character(0), sep = "\n") 
-lineasBlogs<-as.numeric(countLines(BlogsData))
+Blogs <- scan(BlogsData, character(0), sep = "\n", encoding="latin1", skipNul = TRUE)
+LineasBlogs<-as.numeric(countLines(BlogsData))
 
 #Se despliegan las cantidades de líneas para ver con cuantos datos se cuenta
-lineasTwitter
-lineasNews
-lineasBlogs
+LineasTwitter
+LineasNews
+LineasBlogs
 
 #Debido al gran tamaño de los datos, se debe tomar una pequeña muestra para procesarlos
 #Primero, se usa set.seed para que sea reproducible la obtención de la muestra aleatoria
@@ -39,35 +39,43 @@ set.seed(123)
 Porciento <- 5/100
 
 #Obtención de las muestras de Tweets, noticias y blogs
-MuestraTwitter <- sample(lineasTwitter, Porciento*lineasTwitter)
+MuestraTwitter <- sample(Twitter, Porciento*LineasTwitter)
 
-MuestraNews <- sample(lineasNews, Porciento*lineasNews)
+MuestraNews <- sample(News, Porciento*LineasNews)
 
-MuestraBlogs <- sample(lineasBlogs, Porciento*lineasBlogs)
+MuestraBlogs <- sample(Blogs, Porciento*LineasBlogs)
 
 #---------------------- Limpieza y Preprocesamiento ---------------------------------#
 #Primero, se unifican los datos en un solo objeto
-Datos <- paste(MuestraTwitter,MuestraNews,MuestraBlogs)  
-Datos <- sent_detect(Datos, language = "en", model = NULL) 
+Datos <- paste(MuestraTwitter,MuestraNews,MuestraBlogs) 
+Datos <- sent_detect(Datos, language = "en", model = NULL)
 
 #Se realiza un vector de los datos y se convierten en volátiles para cambiar su contenido
 VectorDatos <- VectorSource(Datos)
 DatosLimpios <- VCorpus(VectorDatos)
 
 #Preparar transformación
-DatosLimpios <- tm_map(DatosLimpios,  content_transformer(function(x) iconv(x, to='UTF-8', sub='byte')))
+DatosLimpios <- tm_map(DatosLimpios, content_transformer(function(x) iconv(x, to='UTF-8', sub='byte')))
 
 #Se transforman los caracteres a minúsculas
 DatosLimpios<-tm_map(DatosLimpios, content_transformer(tolower))
 
 #Se eliminan los espacios en blanco adicionales
-DatosLimpios<-tm_map(DatosLimpios, stripWhitespace)
+DatosLimpios<-tm_map(DatosLimpios, content_transformer(stripWhitespace))
+
+#Se eliminan los URLs, se detiene al encontrar un espacio
+RemoverURL <- content_transformer(function(x) gsub("(f|ht)tp(s?)://\\S+", "", x, perl=T))
+DatosLimpios <- tm_map(DatosLimpios, RemoverURL)
+Espacio <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+DatosLimpios <- tm_map(DatosLimpios, Espacio, "/")
+DatosLimpios <- tm_map(DatosLimpios, Espacio, "@")
+DatosLimpios <- tm_map(DatosLimpios, Espacio, "\\|")
 
 #Se eliminan las puntuaciones y símbolos
-DatosLimpios<-tm_map(DatosLimpios, removePunctuation)
+DatosLimpios<-tm_map(DatosLimpios, content_transformer(removePunctuation))
 
 #Se eliminan los números para que no interfieran con la predicción
-DatosLimpios<-tm_map(DatosLimpios, removeNumbers)
+DatosLimpios<-tm_map(DatosLimpios, content_transformer(removeNumbers))
 
 #Se eliminan artículos, preposiciones y conjunciones (stopwords)
 DatosLimpios<-tm_map(DatosLimpios, removeWords, stopwords('english'))
@@ -77,47 +85,59 @@ MP <- file("./data/profanities.txt", "r")
 MPVector <- VectorSource(readLines(MP))
 DatosLimpios <- tm_map(DatosLimpios, removeWords, MPVector)
 
-#Eliminación de url
-DatosLimpios <- gsub("http\\w+","", DatosLimpios)
+#------------------------ Tokenización y ngramas -----------------------------------#
 
-#------------------------ Tokenización con ngramas -----------------------------------#
+#Tokenización de Unigramas
+UnigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=1, max=1))
 
 #Tokenización de Bigramas
-BigramToken <- function(x) unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
+BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=2, max=2))
 
 #Tokenización de Trigramas
-TrigramToken <- function(x) unlist(lapply(ngrams(words(x), 3), paste, collapse = " "), use.names = FALSE)
+TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=3, max=3))
 
 #Función para obtener las frecuencias de cada palabra
 Frecuencia <- function(tdm){
-  freq <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
-  Frecuencia <- data.frame(word=names(freq), freq=freq)
+  Frec <- sort(rowSums(as.matrix(tdm)), decreasing=TRUE)
+  Frecuencia <- data.frame(word=names(Frec), Frec=Frec)
   return(Frecuencia)
 }
 
-#Remoción de palabras que aparecen menos de una vez
-Unigrama <- removeSparseTerms(TermDocumentMatrix(DatosLimpios), 0.9999)
+#Remoción de unigramas que reaparecen menos de una vez
+Unigrama <- removeSparseTerms(TermDocumentMatrix(DatosLimpios, control = list(tokenize = UnigramTokenizer)), 0.998)
 
 #Se obtienen las frecuencias de los unigramas
 FrecUnigrama <- Frecuencia(Unigrama)
 
-#Remoción de palabras que aparecen menos de una vez
-Bigrama <- removeSparseTerms(TermDocumentMatrix(DatosLimpios, control = list(tokenize = BigramToken)), 0.9999)
+#Remoción de bigramas que reaparecen menos de una vez
+Bigrama <- removeSparseTerms(TermDocumentMatrix(DatosLimpios, control = list(tokenize = BigramTokenizer)), 0.9999)
 
 #Se obtienen las frecuencias de los bigramas
 FrecBigrama <- Frecuencia(Bigrama)
 
-#Remoción de palabras que aparecen menos de una vez
-Trigrama <- removeSparseTerms(TermDocumentMatrix(DatosLimpios, control = list(tokenize = TrigramToken)), 0.9999)
+#Remoción de trigramas que reaparecen menos de una vez
+Trigrama <- removeSparseTerms(TermDocumentMatrix(DatosLimpios, control = list(tokenize = TrigramTokenizer)), 0.9999)
 
 #Se obtienen las frecuencias de los trigramas
 FrecTrigrama <- Frecuencia(Trigrama)
 
-#Graficación de los datos
+#Función para graficación de los datos
 PlotFrec <- function(data, title) {
-  ggplot(data[1:25,], aes(reorder(word, -freq), freq)) +
+  ggplot(data[1:25,], aes(reorder(word, -Frec), Frec)) +
     labs(x = "Palabras/Frases", y = "Frecuencia") +
     ggtitle(title) +
     theme(axis.text.x = element_text(angle = 90, size = 12, hjust = 1)) +
     geom_bar(stat = "identity")
 }
+
+#--------------------------- Gráficas de frecuencia --------------------------------------#
+
+#Unigramas
+PlotFrec(FrecUnigrama, "Unigramas más comúnes (Top 25)")
+
+#Bigramas
+PlotFrec(FrecBigrama, "Bigramas más comúnes (Top 25)")
+
+#Trigramas
+PlotFrec(FrecTrigrama, "Trigramas más comúnes (Top 25)")
+
